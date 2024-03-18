bb46b2ae41b78031c72dc2f8fb61d81c2a4be756
Najib93 <161585943+Najib93@users.noreply.github.com>
2024-03-18 16:34:31 +0400

    Add files via upload

---
+++ task6.py
@@ -0,0 +1,73 @@
+# 1#
+# newlist=[1,2,3,4,5,6,7,8,9]
+# i=0
+# while(i < len(newlist)):
+#     print("i: ",i,"Sayı: ",newlist[i])
+#     i=i+1
+    
+# print("Döngü bitti.")
+
+
+2#
+# letter1=["a","m","u","e","t","o"]
+# letter2=("a","e")
+# index = 0
+# while index < len(letter1):  
+#     if letter1[index] in letter2:
+#         index +=1
+#         continue
+#     print(letter1[index])
+#     index +=1
+
+# task3
+
+# n = int(input("Bir ədəd daxil edin: "))
+
+# for i in range(n + 1):
+#     print(i)
+
+
+
+# task4
+
+
+# static_password = "gizli123"
+# remaining_attempts = 3
+
+# for attempt in range(1, 4):
+#     password = input("Parolu daxil edin: ")
+
+#     if password == static_password:
+#         print("Daxil etdiyiniz parol düzgündür!")
+#         break
+#     else:
+#         remaining_attempts -= 1
+#         if remaining_attempts == 0:
+#             print("Block olundunuz!")
+#             break
+#         else:
+#             print(f"Yanlış parol. Qalan cəhdləriniz: {remaining_attempts}")
+
+
+# task5
+
+# correct_answers = 0
+# wrong_answers = 0
+# remaining_removals = 4
+
+# Sorular və cavablar
+# questions = ["Paytaxtimiz hansi şəhərdir?", "İlk insan kimdir?", "Python dilinin yaradicisi kimdir?"]
+# answers = ["Baki", "Adam", "Guido van Rossum"]
+
+
+
+
+
+
+
+    
+
+
+
+
+
